69f779bead7a57b49295388f764bd1ecaa847635
Juan Batiz-Benet <juan@benet.ai>
2014-11-08 20:42:38 -0800

    cleanup lingering procs

--- ucat/test_simple.sh
+++ ucat/test_simple.sh
@@ -19,22 +19,31 @@ test_send() {
   # dialer sends
   log "sending from dialer"
   ./ucat -v $addr 2>&1 <${file}expected | sed "s/^/  dialer1: /" &
+  dialer=$!
   ./ucat -v -l $addr 2>&1 >${file}actual1 | sed "s/^/listener1: /"
+  listener=$!
+
   diff ${file}expected ${file}actual1
   if test $? != 0; then
     log "sending from dialer failed. compare with:\n"
     log "diff ${file}expected ${file}actual1"
+    kill -9 $dialer
+    kill -9 $listener
     exit 1
   fi
 
   # listener sends
   log "sending from listener"
   ./ucat -v -l $addr 2>&1 <${file}expected | sed "s/^/listener2: /" &
+  dialer=$!
   ./ucat -v $addr 2>&1 >${file}actual2 | sed "s/^/  dialer2: /"
+  listener=$!
   diff ${file}expected ${file}actual2
   if test $? != 0; then
     log "sending from listener failed. compare with:\n"
     log "diff ${file}expected ${file}actual2"
+    kill -9 $dialer
+    kill -9 $listener
     exit 1
   fi
 
@@ -43,7 +52,7 @@ test_send() {
   return 0
 }
 
-
+mkdir -p ".trash"
 test_send ".trash/1KB" 1024
 test_send ".trash/1MB" 1048576
 test_send ".trash/1GB" 1073741824
